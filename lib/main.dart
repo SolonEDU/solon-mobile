b226e35802c7ae5c3b2ec6299bc48a7261bd969d
ericlau00 <sharkguy418@gmail.com>
2019-08-22 22:59:32 -0400

    nav bar

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import './navbar.dart';
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Solon';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Proposals',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Events',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3 Forum',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return DecoratedBox(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage('images/solon.png'),
+              )),
+            );
+          },
+        ),
+        title: const Text('Solon'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: NavBar(_selectedIndex, _onItemTapped),
+    );
+  }
+}
---
+++ lib/navbar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function onItemTapped;
+
+  NavBar(this.selectedIndex, this.onItemTapped);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          title: Text('Home'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today),
+          title: Text('Proposals'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.event),
+          title: Text('Events'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.forum),
+          title: Text('Forum'),
+        )
+      ],
+      currentIndex: selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: onItemTapped,
+    );
+  }
+}
