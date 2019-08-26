cba2553ea16eba56d9b5a348afd913ba4a8dae2c
Kazi Jamal <jamalkazi02@gmail.com>
2019-08-26 16:25:45 -0400

    added welcome page and sign up

---
+++ lib/auth/sign_in.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../main.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email, _password;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(),
+      body: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'Please type an email';
+                  }
+                  return null;
+                },
+                onSaved: (input) => _email = input,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              TextFormField(
+                validator: (input) {
+                  if (input.length < 6) {
+                    return 'Your password needs to be at least 6 characters';
+                  }
+                  return null;
+                },
+                onSaved: (input) => _password = input,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              RaisedButton(
+                onPressed: signIn,
+                child: Text('Sign In'),
+              )
+            ],
+          )),
+    );
+  }
+
+  Future<void> signIn() async {
+    final formState = _formKey.currentState;
+    if (formState.validate()) {
+      formState.save();
+      try {
+        AuthResult result = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: _email, password: _password);
+        FirebaseUser user = result.user;
+        if (!user.isEmailVerified) {
+          _showToast('Email is not verified');
+        } else {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Main(),
+            ),
+          );
+        }
+      } catch (e) {
+        _showToast(e.message);
+      }
+    }
+  }
+
+  void _showToast(String message) {
+    _scaffoldKey.currentState.showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+}
