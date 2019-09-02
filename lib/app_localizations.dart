26191fcc4c98a457ec7fbb949c0b0373b49bc234
ray. lee <rlee00@stuy.edu>
2019-09-02 18:02:37 -0400

    added languages fixed title/desc error; added ukrainian

--- lib/app_localizations.dart
+++ lib/app_localizations.dart
@@ -21,7 +21,7 @@ class AppLocalizations {
   Future<bool> load() async {
     String jsonString =
         await rootBundle.loadString('lang/${locale.languageCode}.json');
-    print('${locale.languageCode} ${locale.countryCode} printed from app localizations');
+    print('${locale.languageCode} printed from app localizations ${locale.countryCode}');
     Map<String, dynamic> jsonMap = json.decode(jsonString);
 
     _localizedStrings = jsonMap.map(
