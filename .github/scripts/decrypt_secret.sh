4585e619dae606a448d37c29542d99335a0f5786
ericlau00 <ericlau41802@gmail.com>
2019-11-27 22:43:24 -0500

    actually so dumb

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,4 +2,4 @@
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output ./android/app/my_secret.json ./android/app/my_secret.json.gpg
+--output ./android/app/google-services.json ./android/app/google-services.json.gpg
