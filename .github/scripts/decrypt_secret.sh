249e3b44acbb259cc4d20b68664995bb659debb6
ericlau00 <ericlau41802@gmail.com>
2019-11-27 22:24:52 -0500

    update fork

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
+--output $HOME/android/app/my_secret.json $HOME/android/app/my_secret.json.gpg
