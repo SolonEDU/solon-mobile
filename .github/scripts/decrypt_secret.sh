1ae8ca566e9eea1d1d7462a141d4d3ade8973fd7
ericlau00 <ericlau41802@gmail.com>
2019-11-27 22:39:01 -0500

    change relative root

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,4 +2,4 @@
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output $GITHUB_WORKSPACE/android/app/my_secret.json $GITHUB_WORKSPACE/android/app/my_secret.json.gpg
+--output ./android/app/my_secret.json ./android/app/my_secret.json.gpg
