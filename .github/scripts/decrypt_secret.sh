a49782acd0607c51fdfbb263c6532669a07a5cb4
ericlau00 <ericlau41802@gmail.com>
2019-11-27 22:31:37 -0500

    change filesystem

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,4 +2,4 @@
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output $HOME/android/app/my_secret.json $HOME/android/app/my_secret.json.gpg
+--output $GITHUB_WORKSPACE/android/app/my_secret.json $GITHUB_WORKSPACE/android/app/my_secret.json.gpg
