d840416c7b5392cce49ad1d5ee7ba87578d6cbb9
ray. lee <rlee00@stuy.edu>
2019-12-23 16:22:49 -0500

    proposal page dup

--- lib/proposal/page.dart
+++ lib/proposal/page.dart
@@ -1,105 +1,13 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:Solon/api/api_connect.dart';
 import 'package:flutter/material.dart';
-// import 'package:intl/intl.dart';
+import 'package:Solon/api/api_connect.dart';
 import 'package:Solon/app_localizations.dart';
 import 'package:Solon/loader.dart';
-
-// class ProposalPage0 extends StatelessWidget {
-//   final int pid;
-//   final String title;
-//   final String description;
-//   // final double daysLeft;
-//   // final DateTime endDate;
-//   // final int numYea;
-//   // final int numNay;
-//   // final Future<DocumentSnapshot> creator;
-
-//   ProposalPage0(
-//     this.pid,
-//     this.title,
-//     this.description,
-//     // this.daysLeft,
-//     // this.endDate,
-//     // this.numYea,
-//     // this.numNay,
-//     // this.creator,
-//   );
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(title),
-//       ),
-//       body: Container(
-//         width: double.infinity,
-//         child: Padding(
-//           padding: const EdgeInsets.all(8.0),
-//           child: Column(
-//             children: <Widget>[
-//               // FutureBuilder(
-//               //   future: creator,
-//               //   builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-//               //     return Text(snapshot.data['name']);
-//               //   },
-//               // ),
-//               Text(description),
-//               Icon(Icons.comment),
-//               Text(AppLocalizations.of(context).translate('votesFor')),
-//               // Text('${AppLocalizations.of(context).translate('yea')}: $numYea'),
-//               // Text('${AppLocalizations.of(context).translate('nay')}: $numNay'),
-//               // Text('Voting on proposal ends on: ' + new DateFormat.yMMMMd("en_US").add_jm().format(endDate)),
-//               // Text('Days left: ' + daysLeft.toInt().toString()),
-//               ButtonBar(
-//                 alignment: MainAxisAlignment.center,
-//                 children: <Widget>[
-//                   FlatButton(
-//                     child: Text(AppLocalizations.of(context).translate('yea')),
-//                     color: pressAttention ? Colors.grey : Colors.blue,
-//                     onPressed: () {
-//                       // widget.numYea++;
-//                       setState(() {
-//                         voteChoiceVisibility = false;
-//                       });
-//                     },
-//                   ),
-//                   FlatButton(
-//                     child: Text(AppLocalizations.of(context).translate('nay')),
-//                     onPressed: () {
-//                       // widget.numNay++;
-//                       setState(() {
-//                         voteChoiceVisibility = false;
-//                       });
-//                     },
-//                   ),
-//                   // FlatButton(
-//                   //   child: Icon(Icons.delete),
-//                   //   onPressed: () {
-//                   //     APIConnect.deleteProposal(widget.pid);
-//                   //     // collection.document(widget.doc.documentID).delete();
-//                   //   },
-//                   // ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 class ProposalPage extends StatefulWidget {
   final int pid;
   final String title;
   final String description;
   final int uidUser;
-  // final double daysLeft;
-  // final DateTime endDate;
-  // final int numYea;
-  // final int numNay;
-  // final Future<DocumentSnapshot> creator;
 
   ProposalPage({
     Key key,
@@ -107,40 +15,35 @@ class ProposalPage extends StatefulWidget {
     this.title,
     this.description,
     this.uidUser,
-    // this.daysLeft,
-    // this.endDate,
-    // this.numYea,
-    // this.numNay,
-    // this.creator,
   }) : super(key: key);
+
   @override
   _ProposalPageState createState() => _ProposalPageState();
 }
 
 class _ProposalPageState extends State<ProposalPage> {
-  bool pressAttention = false;
-  int voteVal;
-  String voteOutput;
-  String httpReq;
-  bool voteVisibility;
+  String _voteOutput;
+  Future<Map<String, dynamic>> _listFutureProposal;
 
-  Future<void> vote(int pid, int uidUser, int voteVal) async {
-    final responseMessage = await APIConnect.connectVotes('POST',
-        pid: pid, uidUser: uidUser, voteVal: voteVal);
-    // print(responseMessage['message']);
-  }
-
-  Future<Map<String, dynamic>> getVote(int pid, int uidUser) async {
+  Future<Map<String, dynamic>> getVote() async {
     final responseMessage = await APIConnect.connectVotes(
       'GET',
-      pid: pid,
-      uidUser: uidUser,
+      pid: widget.pid,
+      uidUser: widget.uidUser,
     );
     return responseMessage;
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    _listFutureProposal = getVote();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(widget.pid);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -150,105 +53,113 @@ class _ProposalPageState extends State<ProposalPage> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: FutureBuilder<Map<String, dynamic>>(
-              future: getVote(widget.pid, widget.uidUser),
-              builder: (context, snapshot) {
+              future: _listFutureProposal,
+              builder: (BuildContext context,
+                  AsyncSnapshot<Map<String, dynamic>> snapshot) {
                 if (snapshot.data == null) {
                   return Center(
                     child: Loader(),
                   );
                 }
                 if (snapshot.data['message'] == 'Error') {
-                  voteOutput = "You have not voted yet!";
-                  voteVal = null;
-                  httpReq = 'POST';
-                  voteVisibility = true;
+                  _voteOutput = "You have not voted yet!";
                 } else {
-                  voteOutput = "${snapshot.data['message']}";
-                  voteVal = snapshot.data['vote']['value'];
-                  voteOutput = voteVal == 1
+                  _voteOutput = snapshot.data['vote']['value'] == 1
                       ? "You have voted Yea!"
                       : "You have voted Nay!";
-                  pressAttention = voteVal == 1 ? false : true;
-                  print(voteVal);
-                  httpReq = 'PATCH';
-                  voteVisibility = false;
                 }
                 print(snapshot.data['message']);
-                return Column(
-                  //MIGHT convert to ListView
+                return ListView(
                   children: <Widget>[
-                    // FutureBuilder(
-                    //   future: creator,
-                    //   builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-                    //     return Text(snapshot.data['name']);
-                    //   },
-                    // ),
                     Text(widget.description),
-                    Icon(Icons.comment),
                     Text(AppLocalizations.of(context).translate('votesFor')),
-                    Text(voteOutput),
-                    // Text('${AppLocalizations.of(context).translate('yea')}: $numYea'),
-                    // Text('${AppLocalizations.of(context).translate('nay')}: $numNay'),
-                    // Text('Voting on proposal ends on: ' + new DateFormat.yMMMMd("en_US").add_jm().format(endDate)),
-                    // Text('Days left: ' + daysLeft.toInt().toString()),
-                    Visibility(
-                      visible: voteVisibility,
-                      child: ButtonBar(
-                        alignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          FlatButton(
-                            child: Text(
-                                AppLocalizations.of(context).translate('yea')),
-                            color: Colors.blue,
-                            // voteVal == null
-                            //     ? Colors.white
-                            //     : pressAttention ? Colors.white : Colors.blue,
-                            onPressed: () {
-                              // widget.numYea++;
-                              setState(() {
-                                voteVisibility = false;
-                                voteVal = 1;
-                                pressAttention = pressAttention;
-                                // voteVal = pressAttention == true ? null : 0;
-                                // voteChoiceVisibility = false;
-                              });
-                              vote(widget.pid, widget.uidUser, 1);
-                            },
-                          ),
-                          FlatButton(
-                            child: Text(
-                                AppLocalizations.of(context).translate('nay')),
-                            color: Colors.blue, 
-                            // voteVal == null
-                            //     ? Colors.white
-                            //     : !pressAttention ? Colors.white : Colors.blue,
-                            onPressed: () {
-                              // widget.numNay++;
-                              voteVisibility = false;
-                              setState(() {
-                                voteVal = 0;
-                                // voteVal = pressAttention == false ? null : 1;
-                                pressAttention = true;
-                                // voteChoiceVisibility = false;
-                              });
-                              vote(widget.pid, widget.uidUser, 0);
-                            },
-                          ),
-                          // FlatButton(
-                          //   child: Icon(Icons.delete),
-                          //   onPressed: () {
-                          //     APIConnect.deleteProposal(widget.pid);
-                          //     // collection.document(widget.doc.documentID).delete();
-                          //   },
-                          // ),
-                        ],
-                      ),
-                    ),
+                    snapshot.data['message'] == 'Error'
+                        ? PreventDoubleTap(
+                            pid: widget.pid,
+                            uidUser: widget.uidUser,
+                            voted: snapshot.data['message'] == 'Error'
+                                ? false
+                                : true,
+                          )
+                        : Text(_voteOutput),
                   ],
                 );
               }),
         ),
       ),
     );
+  }
+}
+
+class PreventDoubleTap extends StatefulWidget {
+  final int pid;
+  final int uidUser;
+  final bool voted;
+
+  PreventDoubleTap({
+    Key key,
+    this.pid,
+    this.uidUser,
+    this.voted,
+  }) : super(key: key);
+
+  @override
+  PreventDoubleTapState createState() {
+    return new PreventDoubleTapState();
+  }
+}
+
+class PreventDoubleTapState extends State<PreventDoubleTap> {
+  //boolean value to determine whether button is tapped
+  bool _isButtonTapped = false;
+
+  Future<Map<String, dynamic>> getVote() async {
+    final responseMessage = await APIConnect.connectVotes(
+      'GET',
+      pid: widget.pid,
+      uidUser: widget.uidUser,
+    );
+    return responseMessage;
+  }
+
+  Future<void> vote(int pid, int uidUser, int voteVal) async {
+    APIConnect.connectVotes('POST',
+        pid: pid, uidUser: uidUser, voteVal: voteVal);
+    // print(responseMessage['message']);
+  }
+
+  _onYeaTapped() async {
+    setState(() => _isButtonTapped =
+        !_isButtonTapped); //tapping the button once, disables the button from being tapped again
+    print('is button tapped? yea ${_isButtonTapped}');
+    vote(widget.pid, widget.uidUser, 1);
+  }
+
+  _onNayTapped() async {
+    setState(() => _isButtonTapped =
+        !_isButtonTapped); //tapping the button once, disables the button from being tapped again
+    print('is button tapped? nay ${_isButtonTapped}');
+    vote(widget.pid, widget.uidUser, 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('is button tapped? build ${_isButtonTapped}');
+    return ButtonBar(alignment: MainAxisAlignment.center, children: <Widget>[
+      RaisedButton(
+        color: Colors.red,
+        child: Text('Tap Once'),
+        onPressed: _isButtonTapped
+            ? null
+            : _onYeaTapped, //if button hasnt being tapped, allow user tapped. else, dont allow
+      ),
+      RaisedButton(
+        color: Colors.red,
+        child: Text('Tap Once'),
+        onPressed: _isButtonTapped
+            ? null
+            : _onNayTapped, //if button hasnt being tapped, allow user tapped. else, dont allow
+      ),
+    ]);
   }
 }
