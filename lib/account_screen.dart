eca7dff2b50eeca0e8f803d30bde2439c5030115
ray. lee <rlee00@stuy.edu>
2019-12-25 09:33:28 -0500

    logging out works now i think

--- lib/account_screen.dart
+++ lib/account_screen.dart
@@ -141,10 +141,25 @@ class _AccountScreenState extends State<AccountScreen> {
                     RaisedButton(
                       onPressed: () async {
                         // await FirebaseAuth.instance.signOut();
-                        Navigator.pushReplacement(
+                        Navigator.pushAndRemoveUntil(
                             context,
-                            new MaterialPageRoute(
-                                builder: (context) => new MyApp()));
+                            PageRouteBuilder(pageBuilder: (BuildContext context,
+                                Animation animation,
+                                Animation secondaryAnimation) {
+                              return MyApp();
+                            }, transitionsBuilder: (BuildContext context,
+                                Animation<double> animation,
+                                Animation<double> secondaryAnimation,
+                                Widget child) {
+                              return new SlideTransition(
+                                position: new Tween<Offset>(
+                                  begin: const Offset(1.0, 0.0),
+                                  end: Offset.zero,
+                                ).animate(animation),
+                                child: child,
+                              );
+                            }),
+                            (Route route) => false);
                       },
                       child: Text("Log Out"),
                     ),
