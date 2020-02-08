101df8eab4b539e74052daf4a1c32691afdff784
raymondlee00 <rlee00@stuy.edu>
2020-02-07 22:00:02 -0500

    added dummy dropdownmenu

--- lib/proposal/screen.dart
+++ lib/proposal/screen.dart
@@ -33,33 +33,61 @@ class _ProposalsScreenState extends State<ProposalsScreen> with Screen {
 
   @override
   Widget build(BuildContext context) {
+    String dropdownValue = 'One';
     return RefreshIndicator(
       onRefresh: getStream,
-      child: StreamBuilder<List<ProposalCard>>(
-        stream: APIConnect.proposalListView,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) return Text("${snapshot.error}");
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return Scaffold(
-                body: Center(
-                  child: CircularProgressIndicator(),
-                ),
+      child: Column(
+        children: <Widget>[
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String newValue) {
+              setState(() {
+                dropdownValue = newValue;
+              });
+            },
+            items: <String>['One', 'Two', 'Free', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
               );
-            default:
-              return Scaffold(
-                body: ListView(
-                  padding: const EdgeInsets.all(4),
-                  children: snapshot.data,
-                ),
-                floatingActionButton: getFAB(
-                  context,
-                  CreateProposal(APIConnect.addProposal),
-                  getStream,
-                ),
-              );
-          }
-        },
+            }).toList(),
+          ),
+          StreamBuilder<List<ProposalCard>>(
+            stream: APIConnect.proposalListView,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) return Text("${snapshot.error}");
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return Scaffold(
+                    body: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                default:
+                  return Scaffold(
+                    body: ListView(
+                      padding: const EdgeInsets.all(4),
+                      children: snapshot.data,
+                    ),
+                    floatingActionButton: getFAB(
+                      context,
+                      CreateProposal(APIConnect.addProposal),
+                      getStream,
+                    ),
+                  );
+              }
+            },
+          ),
+        ],
       ),
     );
   }
