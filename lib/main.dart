b42f29a299dc0b17380b8406ec4a739b1920739a
ray. lee <rlee00@stuy.edu>
2020-02-08 16:13:23 -0500

    removed keys

--- lib/main.dart
+++ lib/main.dart
@@ -187,27 +187,6 @@ class _MainState extends State<Main> {
         'widget': ForumScreen(uid: widget.uid),
       },
     ];
-    final riKey0 = const Key('__RIKEY0__');
-    final riKey1 = const Key('__RIKEY1__');
-    final riKey2 = const Key('__RIKEY2__');
-    final riKey3 = const Key('__RIKEY3__');
-    Key screenKey;
-    switch (_selectedIndex) {
-      case 0:
-        screenKey = riKey0;
-        break;
-      case 1:
-        screenKey = riKey1;
-        break;
-      case 2:
-        screenKey = riKey2;
-        break;
-      case 3:
-        screenKey = riKey3;
-        break;
-      default:
-        screenKey = riKey0;
-    }
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
