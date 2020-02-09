bf1b680aa9bf3db0d9c1dd6ab9467895f8774956
ray. lee <rlee00@stuy.edu>
2020-02-09 13:45:50 -0500

    added switch for event screen connection state

--- lib/event/screen.dart
+++ lib/event/screen.dart
@@ -52,95 +52,111 @@ class _EventsScreenState extends State<EventsScreen> with Screen {
     return StreamBuilder(
         stream: dropdownMenuStreamController.stream,
         builder: (context, optionVal) {
-          return RefreshIndicator(
-            key: _refreshIndicatorKey,
-            onRefresh: () => APIConnect.connectEvents(
-              uid: widget.uid,
-              query: optionVal.data,
-            ),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(right: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Text("Sort by: "),
-                      DropdownButton<String>(
-                        value: optionVal.data,
-                        icon: Icon(Icons.arrow_downward),
-                        iconSize: 24,
-                        elevation: 8,
-                        style: TextStyle(color: Colors.black),
-                        underline: Container(
-                          height: 2,
-                          color: Colors.pink[400],
-                        ),
-                        onChanged: (String newValue) async {
-                          dropdownMenuStreamController.sink.add(newValue);
-                          final prefs = await SharedPreferences.getInstance();
-                          prefs.setString(
-                            'eventsSortOption',
-                            newValue,
-                          );
-                        },
-                        items: <String>[
-                          'Newly created',
-                          'Oldest created',
-                          'Attendees: High to Low',
-                          'Attendees: Low to High',
-                        ].map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
+          switch (optionVal.connectionState) {
+            case ConnectionState.waiting:
+              return SizedBox(
+                //TODO: can be abstracted
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              );
+            default:
+              return RefreshIndicator(
+                key: _refreshIndicatorKey,
+                onRefresh: () => APIConnect.connectEvents(
+                  uid: widget.uid,
+                  query: optionVal.data,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(right: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text("Sort by: "),
+                          DropdownButton<String>(
+                            value: optionVal.data,
+                            icon: Icon(Icons.arrow_downward),
+                            iconSize: 24,
+                            elevation: 8,
+                            style: TextStyle(color: Colors.black),
+                            underline: Container(
+                              height: 2,
+                              color: Colors.pink[400],
+                            ),
+                            onChanged: (String newValue) async {
+                              dropdownMenuStreamController.sink.add(newValue);
+                              final prefs =
+                                  await SharedPreferences.getInstance();
+                              prefs.setString(
+                                'eventsSortOption',
+                                newValue,
+                              );
+                            },
+                            items: <String>[
+                              'Newly created',
+                              'Oldest created',
+                              'Attendees: High to Low',
+                              'Attendees: Low to High',
+                            ].map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: StreamBuilder(
-                    stream: Function.apply(
-                      APIConnect.eventListView,
-                      [
-                        widget.uid,
-                        optionVal.data,
-                      ],
                     ),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasError)
-                        return Text('Error: ${snapshot.error}');
-                      switch (snapshot.connectionState) {
-                        case ConnectionState.waiting:
-                          return SizedBox(
-                            width: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.height,
-                            child: Scaffold(
-                              body: Center(
-                                child: CircularProgressIndicator(),
-                              ),
-                            ),
-                          );
-                        default:
-                          return SizedBox(
-                            width: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.height,
-                            child: Scaffold(
-                              key: _scaffoldKey,
-                              body: ListView(
-                                padding: const EdgeInsets.all(4),
-                                children: snapshot.data,
-                              ),
-                            ),
-                          );
-                      }
-                    },
-                  ),
+                    Expanded(
+                      child: StreamBuilder(
+                        stream: Function.apply(
+                          APIConnect.eventListView,
+                          [
+                            widget.uid,
+                            optionVal.data,
+                          ],
+                        ),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError)
+                            return Text('Error: ${snapshot.error}');
+                          switch (snapshot.connectionState) {
+                            case ConnectionState.waiting:
+                              return SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                height: MediaQuery.of(context).size.height,
+                                child: Scaffold(
+                                  body: Center(
+                                    child: CircularProgressIndicator(),
+                                  ),
+                                ),
+                              );
+                            default:
+                              return SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                height: MediaQuery.of(context).size.height,
+                                child: Scaffold(
+                                  key: _scaffoldKey,
+                                  body: ListView(
+                                    padding: const EdgeInsets.all(4),
+                                    children: snapshot.data,
+                                  ),
+                                ),
+                              );
+                          }
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          );
+              );
+          }
+          ;
         });
   }
 }
