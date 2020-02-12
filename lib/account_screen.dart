fdd1e121115c779ac1e743da65d1d992be3554fa
ray. lee <rlee00@stuy.edu>
2020-02-12 13:00:06 -0500

    fixed acc screen landscape scroll error

--- lib/account_screen.dart
+++ lib/account_screen.dart
@@ -77,146 +77,153 @@ class _AccountScreenState extends State<AccountScreen> with Screen {
                 ),
                 body: Container(
                   margin: const EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  child: ListView(
+                    shrinkWrap: true,
                     children: <Widget>[
-                      Text(
-                        'Name',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Raleway',
-                          fontSize: 25,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 25.0),
-                        child: Text(
-                          '${snapshot.data['firstname']} ${snapshot.data['lastname']}',
-                          style: TextStyle(
-                            fontFamily: 'Raleway',
-                            fontSize: 20,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Name',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Raleway',
+                              fontSize: 25,
+                            ),
                           ),
-                        ),
-                      ),
-                      Text(
-                        'Email',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Raleway',
-                          fontSize: 25,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 25.0),
-                        child: Text(
-                          "${snapshot.data['email']}",
-                          style: TextStyle(
-                            fontFamily: 'Raleway',
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        'Language',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Raleway',
-                            fontSize: 25),
-                      ),
-                      DropdownButton<String>(
-                        value: _language,
-                        isExpanded: true,
-                        onChanged: (String newValue) async {
-                          Map<String, dynamic> newMap = snapshot.data;
-                          newMap['lang'] = newValue;
-                          streamController.sink.add(newMap);
-                          final prefs = await SharedPreferences.getInstance();
-                          final userData = prefs.getString('userData');
-                          final userDataJson = json.decode(userData);
-                          userDataJson['lang'] = newValue;
-                          prefs.setString(
-                            'userData',
-                            json.encode(userDataJson),
-                          );
-                          Message responseMessage =
-                              await APIConnect.changeLanguage(
-                            uid: snapshot.data['uid'],
-                            updatedLang: json
-                                .decode(prefs.getString('userData'))['lang'],
-                          );
-                          showToast(
-                              responseMessage.message == 'Error'
-                                  ? 'Language could not be changed to $newValue'
-                                  : "Language was successfully changed to $newValue",
-                              _scaffoldKey);
-                        },
-                        items: <String>[
-                          'English',
-                          'Chinese (Simplified)',
-                          'Chinese (Traditional)',
-                          'Bengali',
-                          'Korean',
-                          'Russian',
-                          'Japanese',
-                          'Ukrainian'
-                        ].map<DropdownMenuItem<String>>(
-                          (String value) {
-                            Map<String, String> nativeLangNames = {
-                              'English': 'English',
-                              'Chinese (Simplified)': '中文（简体）',
-                              'Chinese (Traditional)': '中文（繁体）',
-                              'Bengali': 'বাংলা',
-                              'Korean': '한국어',
-                              'Russian': 'Русский язык',
-                              'Japanese': '日本語',
-                              'Ukrainian': 'українська мова',
-                            };
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(nativeLangNames[value]),
-                            );
-                          },
-                        ).toList(),
-                      ),
-                      Center(
-                        child: Button(
-                          color: Colors.pink[200],
-                          height: 55,
-                          width: 155,
-                          label: "Sign Out",
-                          margin: EdgeInsets.only(top: 10),
-                          function: () async {
-                            final prefs = await SharedPreferences.getInstance();
-                            prefs.clear();
-                            Navigator.pushAndRemoveUntil(
-                              context,
-                              PageRouteBuilder(
-                                pageBuilder: (
-                                  BuildContext context,
-                                  Animation animation,
-                                  Animation secondaryAnimation,
-                                ) {
-                                  return Solon();
-                                },
-                                transitionsBuilder: (
-                                  BuildContext context,
-                                  Animation<double> animation,
-                                  Animation<double> secondaryAnimation,
-                                  Widget child,
-                                ) {
-                                  return new SlideTransition(
-                                    position: new Tween<Offset>(
-                                      begin: const Offset(1.0, 0.0),
-                                      end: Offset.zero,
-                                    ).animate(animation),
-                                    child: child,
-                                  );
-                                },
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 25.0),
+                            child: Text(
+                              '${snapshot.data['firstname']} ${snapshot.data['lastname']}',
+                              style: TextStyle(
+                                fontFamily: 'Raleway',
+                                fontSize: 20,
                               ),
-                              (Route route) => false,
-                            );
-                          },
-                        ),
+                            ),
+                          ),
+                          Text(
+                            'Email',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Raleway',
+                              fontSize: 25,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 25.0),
+                            child: Text(
+                              "${snapshot.data['email']}",
+                              style: TextStyle(
+                                fontFamily: 'Raleway',
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'Language',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Raleway',
+                                fontSize: 25),
+                          ),
+                          DropdownButton<String>(
+                            value: _language,
+                            isExpanded: true,
+                            onChanged: (String newValue) async {
+                              Map<String, dynamic> newMap = snapshot.data;
+                              newMap['lang'] = newValue;
+                              streamController.sink.add(newMap);
+                              final prefs =
+                                  await SharedPreferences.getInstance();
+                              final userData = prefs.getString('userData');
+                              final userDataJson = json.decode(userData);
+                              userDataJson['lang'] = newValue;
+                              prefs.setString(
+                                'userData',
+                                json.encode(userDataJson),
+                              );
+                              Message responseMessage =
+                                  await APIConnect.changeLanguage(
+                                uid: snapshot.data['uid'],
+                                updatedLang: json.decode(
+                                    prefs.getString('userData'))['lang'],
+                              );
+                              showToast(
+                                  responseMessage.message == 'Error'
+                                      ? 'Language could not be changed to $newValue'
+                                      : "Language was successfully changed to $newValue",
+                                  _scaffoldKey);
+                            },
+                            items: <String>[
+                              'English',
+                              'Chinese (Simplified)',
+                              'Chinese (Traditional)',
+                              'Bengali',
+                              'Korean',
+                              'Russian',
+                              'Japanese',
+                              'Ukrainian'
+                            ].map<DropdownMenuItem<String>>(
+                              (String value) {
+                                Map<String, String> nativeLangNames = {
+                                  'English': 'English',
+                                  'Chinese (Simplified)': '中文（简体）',
+                                  'Chinese (Traditional)': '中文（繁体）',
+                                  'Bengali': 'বাংলা',
+                                  'Korean': '한국어',
+                                  'Russian': 'Русский язык',
+                                  'Japanese': '日本語',
+                                  'Ukrainian': 'українська мова',
+                                };
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(nativeLangNames[value]),
+                                );
+                              },
+                            ).toList(),
+                          ),
+                          Center(
+                            child: Button(
+                              color: Colors.pink[200],
+                              height: 55,
+                              width: 155,
+                              label: "Sign Out",
+                              margin: EdgeInsets.only(top: 10),
+                              function: () async {
+                                final prefs =
+                                    await SharedPreferences.getInstance();
+                                prefs.clear();
+                                Navigator.pushAndRemoveUntil(
+                                  context,
+                                  PageRouteBuilder(
+                                    pageBuilder: (
+                                      BuildContext context,
+                                      Animation animation,
+                                      Animation secondaryAnimation,
+                                    ) {
+                                      return Solon();
+                                    },
+                                    transitionsBuilder: (
+                                      BuildContext context,
+                                      Animation<double> animation,
+                                      Animation<double> secondaryAnimation,
+                                      Widget child,
+                                    ) {
+                                      return new SlideTransition(
+                                        position: new Tween<Offset>(
+                                          begin: const Offset(1.0, 0.0),
+                                          end: Offset.zero,
+                                        ).animate(animation),
+                                        child: child,
+                                      );
+                                    },
+                                  ),
+                                  (Route route) => false,
+                                );
+                              },
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
