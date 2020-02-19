2f34488701a1386ca4174b25e7c1e3853b4d3c67
ray. lee <rlee00@stuy.edu>
2020-02-18 20:56:21 -0500

    abstracted ForumSearch

--- lib/forum/screen.dart
+++ lib/forum/screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:Solon/forum/search.dart';
 import 'package:Solon/generated/i18n.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -216,67 +217,4 @@ class _ForumScreenState extends State<ForumScreen> with Screen {
       },
     );
   }
-}
-
-// TODO: move to another file after we're done experimenting
-class ForumSearch extends SearchDelegate {
-  BuildContext context;
-
-  ForumSearch(this.context);
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    if (query == '') return Container();
-    return StreamBuilder(
-      stream: Function.apply(
-        APIConnect.forumSearchListView,
-        [
-          query,
-        ],
-      ),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return Container(
-              child: CircularProgressIndicator(),
-            );
-          default:
-            return ListView(
-              children: snapshot.data,
-            );
-        }
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return Container();
-  }
-
-  @override
-  String get searchFieldLabel => I18n.of(context).searchForum;
 }
