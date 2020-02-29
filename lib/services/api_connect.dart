bf000c43742f303feff961bfd9b5f38d7ef205c1
ray. lee <rlee00@stuy.edu>
2020-02-29 14:05:28 -0500

    Add catch-all error to APIConnect

--- lib/services/api_connect.dart
+++ lib/services/api_connect.dart
@@ -22,11 +22,13 @@ class APIConnect {
     final response = await http.get(url);
     int status = response.statusCode;
     if (status == 200) {
-      Message.fromJson(json.decode(response.body)['message']);
+      return Message.fromJson(json.decode(response.body)['message']);
     } else if (status == 500) {
       throw Exception('Internal Server Error -- We had a problem with our server. Try again later.');
     } else if (status == 504) {
       throw Exception('Gateway Timeout');
+    } else {
+      throw Exception('An unknown error occurred. care.');
     }
   }
 }
